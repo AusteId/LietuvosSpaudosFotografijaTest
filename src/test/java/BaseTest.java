39563a8273182ebee96de643a086d9f66667058c
AusteId <auste.idaite@gmail.com>
2024-03-26 00:44:35 +0200

    Add initializeDriver method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,15 +3,21 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
     protected static WebDriver driver;
 
-
-
-    @BeforeEach
-    void setup() {
-        driver = new ChromeDriver();
+    void initializeDriver(String browser) {
+        
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
         driver.manage().window().maximize();
         driver.get("http://localhost:5173");
     }
