bd263083ffebfffaabd55dd48073961cc8822e9b
AusteId <auste.idaite@gmail.com>
2024-03-27 01:54:16 +0200

    Add getToken method and createCompetition payload

--- src/test/java/APITesting/files/ReusableMethods.java
+++ src/test/java/APITesting/files/ReusableMethods.java
@@ -2,6 +2,8 @@ package APITesting.files;
 
 import io.restassured.path.json.JsonPath;
 
+import static io.restassured.RestAssured.given;
+
 public class ReusableMethods {
 
     public static JsonPath rawToJson(String response) {
@@ -9,5 +11,15 @@ public class ReusableMethods {
         return json;
     }
 
+    public static String getToken(String email, String password) {
 
+        String response = given().log().all().header("Content-Type", "application/json")
+                .body(Payload.loginUser(email, password))
+                .when().post("/api/v1/login")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        String token = rawToJson(response).getString("token");
+
+        return token;
+    }
 }
---
+++ src/test/java/APITesting/testAPI/CompetitionCreationTest.java
@@ -0,0 +1,9 @@
+package APITesting.testAPI;
+
+import static io.restassured.RestAssured.given;
+
+public class CompetitionCreationTest extends BaseTest {
+
+
+    }
+}
